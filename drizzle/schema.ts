981d6943b817d816951ff2a4c480b789aa7f786d
DennisDHBW <den.gerhardt.24@lehre.mosbach.dhbw.de>
2025-10-08 15:09:05 +0200

    drizzle init

---
+++ drizzle/schema.ts
@@ -0,0 +1,65 @@
+import { sqliteTable, AnySQLiteColumn, foreignKey, text, integer, uniqueIndex } from "drizzle-orm/sqlite-core"
+  import { sql } from "drizzle-orm"
+
+export const account = sqliteTable("account", {
+	id: text().primaryKey().notNull(),
+	accountId: text("account_id").notNull(),
+	providerId: text("provider_id").notNull(),
+	userId: text("user_id").notNull().references(() => user.id, { onDelete: "cascade" } ),
+	accessToken: text("access_token"),
+	refreshToken: text("refresh_token"),
+	idToken: text("id_token"),
+	accessTokenExpiresAt: integer("access_token_expires_at"),
+	refreshTokenExpiresAt: integer("refresh_token_expires_at"),
+	scope: text(),
+	password: text(),
+	createdAt: integer("created_at").default(sql`(cast(unixepoch('subsecond') * 1000 as integer))`).notNull(),
+	updatedAt: integer("updated_at").notNull(),
+});
+
+export const demoData = sqliteTable("demo_data", {
+	id: integer().primaryKey().notNull(),
+	header: text().notNull(),
+	type: text().notNull(),
+	status: text().notNull(),
+	target: integer().notNull(),
+	limit: integer().notNull(),
+	reviewer: text().notNull(),
+});
+
+export const session = sqliteTable("session", {
+	id: text().primaryKey().notNull(),
+	expiresAt: integer("expires_at").notNull(),
+	token: text().notNull(),
+	createdAt: integer("created_at").default(sql`(cast(unixepoch('subsecond') * 1000 as integer))`).notNull(),
+	updatedAt: integer("updated_at").notNull(),
+	ipAddress: text("ip_address"),
+	userAgent: text("user_agent"),
+	userId: text("user_id").notNull().references(() => user.id, { onDelete: "cascade" } ),
+},
+(table) => [
+	uniqueIndex("session_token_unique").on(table.token),
+]);
+
+export const user = sqliteTable("user", {
+	id: text().primaryKey().notNull(),
+	name: text().notNull(),
+	email: text().notNull(),
+	emailVerified: integer("email_verified").default(false).notNull(),
+	image: text(),
+	createdAt: integer("created_at").default(sql`(cast(unixepoch('subsecond') * 1000 as integer))`).notNull(),
+	updatedAt: integer("updated_at").default(sql`(cast(unixepoch('subsecond') * 1000 as integer))`).notNull(),
+},
+(table) => [
+	uniqueIndex("user_email_unique").on(table.email),
+]);
+
+export const verification = sqliteTable("verification", {
+	id: text().primaryKey().notNull(),
+	identifier: text().notNull(),
+	value: text().notNull(),
+	expiresAt: integer("expires_at").notNull(),
+	createdAt: integer("created_at").default(sql`(cast(unixepoch('subsecond') * 1000 as integer))`).notNull(),
+	updatedAt: integer("updated_at").default(sql`(cast(unixepoch('subsecond') * 1000 as integer))`).notNull(),
+});
+
